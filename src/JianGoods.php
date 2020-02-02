cd1469aad8603d42fa1b810431430d717e5e156a
jian@ <941920519@qq.com>
2020-02-02 20:42:29 +0800

    提交正式代码测试

---
+++ src/JianGoods.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace JianGoods;
+
+class JianGoods
+{
+    /**
+     * @ 用户没有登陆，数据暂存cookie内
+     * @param $goodsUserCookie
+     */
+    public static function cookie_user($goodsUserCookie)
+    {
+         return true;
+    }
+
+}
---
+++ src/shopping/Shopping.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace JianGoods\shopping;
+
+
+/**
+ * 购物车
+ * Class Shopping
+ * @package JianGood
+ */
+
+class Shopping
+{
+    /**
+     * @ 用户不登陆时，添加商品到购物车暂时保存到 cookie内
+     *
+     */
+    public function setCookie($goodsDate)
+    {
+//        $_COOKIE['']
+    }
+
+
+
+}
